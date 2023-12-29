0374425a9bde617a314bfc0c9ffde61c0fdbfebc
KitoMC <95675413+KitoMine@users.noreply.github.com>
2023-12-29 17:13:07 +0700

    upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,css}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        rectangle1:
+          "url('https://s3-alpha-sig.figma.com/img/762f/ae6b/a64a61d95fe3f59d0a34777722b36521?Expires=1704672000&Signature=IPYvYCH~uZ2cTfCxhpHEFvqN-cAGWVbMGrZfVO4h5UhdfoNyrAM4vhYytV7nlPX~X4prXVmc2YSLMVwi88UGJU5dnbsu26DSAhAqS0i3tdJ4KO4xryE-XgEtHdqJQ9FwK0JjBNEjsuAOaE72hOxLimg3jiBaXKiIml2tfQxjXOxvyMv0Zw8MkZdzcW3eEwQp-Lph4fvzqea45E~IASAOYfEdBmE4ODCAZ2zK4bymvmLzbmwRFDIzdk0c2KIqEkyId2aDMHgjOCoITEjFbbjHBgVTrnrVeYTEKayQlaC46NELvSJ6dl6uKprQGQkvfvTpK71z3nzPq4VtSLbv69yXig__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')",
+      },
+    },
   },
   plugins: [],
 };
